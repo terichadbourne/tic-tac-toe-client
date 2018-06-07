e68bc16d26bf5fdd4090b09ac49566ac70583aab
terichadbourne <terichad@gmail.com>
2018-06-07 15:50:57 -0400

    Removed whitespace
    Removed whitespace to appease the linter

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -3,7 +3,6 @@
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
 
-
 const events = require('./events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
