ff84a49122a3770effd802db0b65f12ca5d23f70
terichadbourne <terichad@gmail.com>
2018-06-07 16:09:51 -0400

    Add option to restart game
    - Add 'start new game' button with associated click handler
    - Add `restartGame` function to reset board contents, current player,
    and active classes
    - Improved comments in `events.js`

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -2,21 +2,36 @@
 
 const store = require('./store')
 
+// all event handlers
 const addHandlers = function () {
   $('.game-cell').on('click', playHere)
+  $('#restart-game').on('click', restartGame)
+}
+
+const restartGame = function (event) {
+  // set current player to x
+  store.currentTurn = 'x'
+  // empty out contents of all game cells
+  $('.game-cell').html('')
+  // set player x active and player o inactive
+  $('#player-x').addClass('active')
+  $('#player-o').removeClass('active')
 }
 
 const playHere = function (event) {
   console.log('Player ', store.currentTurn, ' selected cell ', event.target.id)
+  // if cell was blank, add symbol of current player, then swap players
   if ($(event.target).html() === '') {
     $(event.target).html(store.currentTurn)
     swapTurns()
+  // if cell was occupied, log error and prevent play and turn swap
   } else {
     console.log('That cell is already occupied. Try again!')
   }
 }
 
 const swapTurns = function () {
+  // swap value of store.currentTurn and toggle active classes
   if (store.currentTurn === 'x') {
     store.currentTurn = 'o'
     $('.player').toggleClass('active')
