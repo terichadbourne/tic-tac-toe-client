63765f7a0f70193d863bd6cae631234193587579
terichadbourne <terichad@gmail.com>
2018-06-08 15:42:01 -0400

    Add user auth functionality
    - Add API calls to sign up, sign in, change password, and sign out,
    triggered by previously built event handlers
    - On successful sign-in, response from server is captured as
    `store.user`, containing the user's id, email, and authentication token
    - Change password function requires the token to be found at
    `store.user.token`
    - Sign out function also requires the token to be found, but deletes
    `store.user` on success
    
    TODO: Add user-facing success/failure messages
    TODO: Show only relevant auth fields based on current login status

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -15,6 +15,11 @@ const onSignUp = function (event) {
   event.preventDefault()
   console.log('clicked on signup button')
   console.log('event.target of sign-up: ', event.target)
+  const data = getFormFields(event.target)
+  console.log('data in onSignUp is: ', data)
+  authApi.signUp(data)
+    .then(authUi.signUpSuccess)
+    .catch(authUi.signUpError)
 }
 // SIGN UP
 //
@@ -35,6 +40,11 @@ const onSignIn = function (event) {
   event.preventDefault()
   console.log('clicked on sign-in button')
   console.log('event.target of sign-in: ', event.target)
+  const data = getFormFields(event.target)
+  console.log('data in onSignIn is: ', data)
+  authApi.signIn(data)
+    .then(authUi.signInSuccess)
+    .catch(authUi.signInError)
 }
 // SIGN IN
 //
@@ -52,10 +62,29 @@ const onSignIn = function (event) {
 //
 // unsuccessful: HTTP Status of 401 Unauthorized (empty body)
 
+const onChangePassword = function (event) {
+  event.preventDefault()
+  console.log('clicked on change password button')
+  console.log('event.target of change password: ', event.target)
+  const data = getFormFields(event.target)
+  console.log('data in onChangePassword is: ', data)
+  authApi.changePassword(data)
+}
+// CHANGE PASSWORD
+//
+// expected responses:
+//
+// successful: HTTP status of 204 No Content (no body)
+//
+// unsuccessful: HTTP status of 400 Bad Request (no body)
+
 const onSignOut = function (event) {
   event.preventDefault()
   console.log('clicked on sign-out button')
   console.log('event.target of sign-out: ', event.target)
+  authApi.signOut()
+    .then(authUi.signOutSuccess)
+    .catch(authUi.signOutError)
 }
 
 // SIGN OUT
@@ -66,24 +95,10 @@ const onSignOut = function (event) {
 //
 // unsuccessful: HTTP status of 401 Unauthorized (no body)
 
-const onChangePassword = function (event) {
-  event.preventDefault()
-  console.log('clicked on change password button')
-  console.log('event.target of change password: ', event.target)
-}
-// CHANGE PASSWORD
-//
-// expected responses:
-//
-// successful: HTTP status of 204 No Content (no body)
-//
-// unsuccessful: HTTP status of 400 Bad Request (no body)
-
 module.exports = {
   addHandlers: addHandlers,
   onSignIn: onSignIn,
   onSignUp: onSignUp,
   onChangePassword: onChangePassword,
   onSignOut: onSignOut
-
 }
--- assets/scripts/auth/ui.js
+++ assets/scripts/auth/ui.js
@@ -2,13 +2,53 @@
 
 const store = require('../store')
 
+const signUpSuccess = function (response) {
+  console.log('signUpSuccess is ', response)
+}
+
+const signUpError = function (error) {
+  console.log('signUpError is', error)
+}
+
+const signInSuccess = function (response) {
+  console.log('signInSucces response is ', response)
+  store.user = response.user
+  console.log('store.user is ', store.user)
+  console.log('store.user.token is ', store.user.token)
+}
+
+const signInError = function (error) {
+  console.log('signInError is', error)
+}
+
+const changePasswordSuccess = function (response) {
+  // there is not supposed to be a response
+  console.log('changePasswordSuccess response is ', response)
+}
+
+const changePasswordError = function (error) {
+  console.log('changePasswordError is', error)
+}
+
+const signOutSuccess = function (response) {
+  // no response
+  console.log('store.user.token is ', store.user.token)
+  delete store.user
+  console.log('You were successfully signed out')
+  console.log('store.user after deleting it: ', store.user)
+}
+
+const signOutError = function (error) {
+  console.log('signOutError is', error)
+}
+
 module.exports = {
-  // signupSuccess: signupSuccess,
-  // signupError: signupError,
-  // signInSuccess: signInSuccess,
-  // signInError: signInError,
-  // changePasswordSuccess: changePasswordSuccess,
-  // changePasswordError: changePasswordError,
-  // signOutSuccess: signOutSuccess,
-  // signOutError: signOutError
+  signUpSuccess: signUpSuccess,
+  signUpError: signUpError,
+  signInSuccess: signInSuccess,
+  signInError: signInError,
+  changePasswordSuccess: changePasswordSuccess,
+  changePasswordError: changePasswordError,
+  signOutSuccess: signOutSuccess,
+  signOutError: signOutError
 }
