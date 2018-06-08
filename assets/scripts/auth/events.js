b3d7bf2537e8b064e835b59b1d13f21ddda9efbc
terichadbourne <terichad@gmail.com>
2018-06-08 14:24:48 -0400

    Add form fields and event handlers
    - Add form fields for each API transaction, formatted to be compatible
    with `getFormFields` function
    - Add event handlers for each corresponding button
    - Add basic functions to register click and console.log event.target
    for each button

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -5,9 +5,85 @@ const authApi = require('./api')
 const authUi = require('./ui')
 
 const addHandlers = function () {
-
+  $('#signup-form').on('submit', onSignUp)
+  $('#signin-form').on('submit', onSignIn)
+  $('#change-password-form').on('submit', onChangePassword)
+  $('#sign-out-button').on('click', onSignOut)
 }
 
+const onSignUp = function (event) {
+  event.preventDefault()
+  console.log('clicked on signup button')
+  console.log('event.target of sign-up: ', event.target)
+}
+// SIGN UP
+//
+// expected responses:
+//
+// successful: JSON as follows...
+//
+// {
+//   "user": {
+//     "id": 1,
+//     "email": "an@example.email"
+//   }
+// }
+//
+// unsuccessful, HTTP Status of 400 Bad Request (empty body)
+
+const onSignIn = function (event) {
+  event.preventDefault()
+  console.log('clicked on sign-in button')
+  console.log('event.target of sign-in: ', event.target)
+}
+// SIGN IN
+//
+// expected responses:
+//
+// successful: JSON as follows...
+//
+// {
+//   "user": {
+//     "id": 1,
+//     "email": "an@example.email",
+//     "token": "an example authentication token"
+//   }
+// }
+//
+// unsuccessful: HTTP Status of 401 Unauthorized (empty body)
+
+const onSignOut = function (event) {
+  event.preventDefault()
+  console.log('clicked on sign-out button')
+  console.log('event.target of sign-out: ', event.target)
+}
+
+// SIGN OUT
+//
+// expected responses:
+//
+// successful: HTTP status of 204 No Content (no body)
+//
+// unsuccessful: HTTP status of 401 Unauthorized (no body)
+
+const onChangePassword = function (event) {
+  event.preventDefault()
+  console.log('clicked on change password button')
+  console.log('event.target of change password: ', event.target)
+}
+// CHANGE PASSWORD
+//
+// expected responses:
+//
+// successful: HTTP status of 204 No Content (no body)
+//
+// unsuccessful: HTTP status of 400 Bad Request (no body)
+
 module.exports = {
-  addHandlers
+  addHandlers: addHandlers,
+  onSignIn: onSignIn,
+  onSignUp: onSignUp,
+  onChangePassword: onChangePassword,
+  onSignOut: onSignOut
+
 }
